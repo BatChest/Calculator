fde554af8497be435cba8b50e54418074b56bc2b
luisb <barneydino55@gmail.com>
2025-03-14 11:49:34 -0700

    partial layout of calc(buttons no work)

--- calculator.js
+++ calculator.js
@@ -2,6 +2,22 @@ let num1;
 let num2;
 let operationSymbol;
 
+const container = document.getElementById('container');
+
+//Create 3x3 grid for calculator
+createGrid(3);
+
+function createGrid(gridSize) {
+  // create grid cells
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const newDiv = document.createElement('div');
+      newDiv.classList.add('gridChild');
+      container.appendChild(newDiv);
+    }
+  }
+}
+
 function operate(num1, num2, operationSymbol) {
   switch (operationSymbol) {
     case '+':
